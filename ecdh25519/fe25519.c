21ec7a569cb2ad8a9af58a92447ab6adcabe8fa1
Yu <jianao931212@gmail.com>
2025-10-06 00:15:12 +0800

    Squaring

--- ecdh25519/fe25519.c
+++ ecdh25519/fe25519.c
@@ -5,32 +5,29 @@ const fe25519 fe25519_zero = {{0}};
 const fe25519 fe25519_one = {{1}};
 const fe25519 fe25519_two = {{2}};
 
-/* sqrt(-1) */
 const fe25519 fe25519_sqrtm1 = {{0xB0, 0xA0, 0x0E, 0x4A, 0x27, 0x1B, 0xEE, 0xC4, 0x78, 0xE4, 0x2F, 0xAD, 0x06, 0x18, 0x43, 0x2F,
                                  0xA7, 0xD7, 0xFB, 0x3D, 0x99, 0x00, 0x4D, 0x2B, 0x0B, 0xDF, 0xC1, 0x4F, 0x80, 0x24, 0x83, 0x2B}};
 
-/* -sqrt(-1) */
 const fe25519 fe25519_msqrtm1 = {{0x3D, 0x5F, 0xF1, 0xB5, 0xD8, 0xE4, 0x11, 0x3B, 0x87, 0x1B, 0xD0, 0x52, 0xF9, 0xE7, 0xBC, 0xD0,
                                   0x58, 0x28, 0x4, 0xC2, 0x66, 0xFF, 0xB2, 0xD4, 0xF4, 0x20, 0x3E, 0xB0, 0x7F, 0xDB, 0x7C, 0x54}};
 
-/* -1 */
 const fe25519 fe25519_m1 = {{236, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
                              0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x7f}};
 
-static uint32_t equal(uint32_t a, uint32_t b) /* 16-bit inputs */
+static uint32_t equal(uint32_t a, uint32_t b)
 {
-  uint32_t x = a ^ b; /* 0: yes; 1..65535: no */
-  x -= 1;             /* 4294967295: yes; 0..65534: no */
-  x >>= 31;           /* 1: yes; 0: no */
+  uint32_t x = a ^ b;
+  x -= 1;
+  x >>= 31;
   return x;
 }
 
-static uint32_t ge(uint32_t a, uint32_t b) /* 16-bit inputs */
+static uint32_t ge(uint32_t a, uint32_t b)
 {
   uint32_t x = a;
-  x -= (uint32_t)b; /* 0..65535: yes; 4294901761..4294967295: no */
-  x >>= 31;         /* 0: yes; 1: no */
-  x ^= 1;           /* 1: yes; 0: no */
+  x -= (uint32_t)b;
+  x >>= 31;
+  x ^= 1;
   return x;
 }
 
@@ -84,7 +81,6 @@ static void reduce_mul(fe25519 *r)
   }
 }
 
-/* reduction modulo 2^255-19 */
 void fe25519_freeze(fe25519 *r)
 {
   int i;
@@ -109,7 +105,6 @@ void fe25519_unpack(fe25519 *r, const unsigned char x[32])
   r->v[31] &= 127;
 }
 
-/* Assumes input x being reduced below 2^255 */
 void fe25519_pack(unsigned char r[32], const fe25519 *x)
 {
   int i;
@@ -129,7 +124,6 @@ int fe25519_isone(const fe25519 *x)
   return fe25519_iseq(x, &fe25519_one);
 }
 
-// return true if x has LSB set
 int fe25519_isnegative(const fe25519 *x)
 {
   fe25519 t = *x;
@@ -155,7 +149,7 @@ int fe25519_iseq(const fe25519 *x, const fe25519 *y)
 
 void fe25519_cmov(fe25519 *r, const fe25519 *x, unsigned char b)
 {
-  /*if(b) *r = *x;*/
+
   uint32_t mask = 0u - (uint32_t)(b & 1u);
   for (int i = 0; i < 32; ++i)
   {
@@ -164,7 +158,7 @@ void fe25519_cmov(fe25519 *r, const fe25519 *x, unsigned char b)
     r->v[i] = ri ^ (mask & (ri ^ xi));
   }
 }
-/* Add */
+
 void fe25519_cswap(fe25519 *a, fe25519 *b, unsigned char bit)
 {
   uint32_t mask = 0u - (uint32_t)(bit & 1u);
@@ -230,14 +224,45 @@ void fe25519_mul(fe25519 *r, const fe25519 *x, const fe25519 *y)
 
   for (i = 32; i < 63; i++)
     r->v[i - 32] = t[i - 32] + times38(t[i]);
-  r->v[31] = t[31]; /* result now in r[0]...r[31] */
+  r->v[31] = t[31];
 
   reduce_mul(r);
 }
 
 void fe25519_square(fe25519 *r, const fe25519 *x)
 {
-  fe25519_mul(r, x, x);
+  uint32_t t[63];
+
+  for (int k = 0; k <= 62; ++k)
+  {
+
+    int i = (k > 31) ? (k - 31) : 0;
+    int j = (k < 31) ? k : 31;
+
+    uint32_t acc = 0;
+
+    while (i < j)
+    {
+      acc += ((uint32_t)x->v[i] * (uint32_t)x->v[j]) << 1;
+      ++i;
+      --j;
+    }
+
+    if (i == j)
+    {
+      uint32_t xi = (uint32_t)x->v[i];
+      acc += xi * xi;
+    }
+
+    t[k] = acc;
+  }
+
+  for (int i = 32; i < 63; ++i)
+    r->v[i - 32] = t[i - 32] + times38(t[i]);
+
+  r->v[31] = t[31];
+
+  reduce_mul(r);
 }
 
 void fe25519_pow2523(fe25519 *r, const fe25519 *x)
@@ -253,66 +278,66 @@ void fe25519_pow2523(fe25519 *r, const fe25519 *x)
   fe25519 t;
   int i;
 
-  /* 2 */ fe25519_square(&z2, x);
-  /* 4 */ fe25519_square(&t, &z2);
-  /* 8 */ fe25519_square(&t, &t);
-  /* 9 */ fe25519_mul(&z9, &t, x);
-  /* 11 */ fe25519_mul(&z11, &z9, &z2);
-  /* 22 */ fe25519_square(&t, &z11);
-  /* 2^5 - 2^0 = 31 */ fe25519_mul(&z2_5_0, &t, &z9);
+  fe25519_square(&z2, x);
+  fe25519_square(&t, &z2);
+  fe25519_square(&t, &t);
+  fe25519_mul(&z9, &t, x);
+  fe25519_mul(&z11, &z9, &z2);
+  fe25519_square(&t, &z11);
+  fe25519_mul(&z2_5_0, &t, &z9);
 
-  /* 2^6 - 2^1 */ fe25519_square(&t, &z2_5_0);
-  /* 2^10 - 2^5 */ for (i = 1; i < 5; i++)
+  fe25519_square(&t, &z2_5_0);
+  for (i = 1; i < 5; i++)
   {
     fe25519_square(&t, &t);
   }
-  /* 2^10 - 2^0 */ fe25519_mul(&z2_10_0, &t, &z2_5_0);
+  fe25519_mul(&z2_10_0, &t, &z2_5_0);
 
-  /* 2^11 - 2^1 */ fe25519_square(&t, &z2_10_0);
-  /* 2^20 - 2^10 */ for (i = 1; i < 10; i++)
+  fe25519_square(&t, &z2_10_0);
+  for (i = 1; i < 10; i++)
   {
     fe25519_square(&t, &t);
   }
-  /* 2^20 - 2^0 */ fe25519_mul(&z2_20_0, &t, &z2_10_0);
+  fe25519_mul(&z2_20_0, &t, &z2_10_0);
 
-  /* 2^21 - 2^1 */ fe25519_square(&t, &z2_20_0);
-  /* 2^40 - 2^20 */ for (i = 1; i < 20; i++)
+  fe25519_square(&t, &z2_20_0);
+  for (i = 1; i < 20; i++)
   {
     fe25519_square(&t, &t);
   }
-  /* 2^40 - 2^0 */ fe25519_mul(&t, &t, &z2_20_0);
+  fe25519_mul(&t, &t, &z2_20_0);
 
-  /* 2^41 - 2^1 */ fe25519_square(&t, &t);
-  /* 2^50 - 2^10 */ for (i = 1; i < 10; i++)
+  fe25519_square(&t, &t);
+  for (i = 1; i < 10; i++)
   {
     fe25519_square(&t, &t);
   }
-  /* 2^50 - 2^0 */ fe25519_mul(&z2_50_0, &t, &z2_10_0);
+  fe25519_mul(&z2_50_0, &t, &z2_10_0);
 
-  /* 2^51 - 2^1 */ fe25519_square(&t, &z2_50_0);
-  /* 2^100 - 2^50 */ for (i = 1; i < 50; i++)
+  fe25519_square(&t, &z2_50_0);
+  for (i = 1; i < 50; i++)
   {
     fe25519_square(&t, &t);
   }
-  /* 2^100 - 2^0 */ fe25519_mul(&z2_100_0, &t, &z2_50_0);
+  fe25519_mul(&z2_100_0, &t, &z2_50_0);
 
-  /* 2^101 - 2^1 */ fe25519_square(&t, &z2_100_0);
-  /* 2^200 - 2^100 */ for (i = 1; i < 100; i++)
+  fe25519_square(&t, &z2_100_0);
+  for (i = 1; i < 100; i++)
   {
     fe25519_square(&t, &t);
   }
-  /* 2^200 - 2^0 */ fe25519_mul(&t, &t, &z2_100_0);
+  fe25519_mul(&t, &t, &z2_100_0);
 
-  /* 2^201 - 2^1 */ fe25519_square(&t, &t);
-  /* 2^250 - 2^50 */ for (i = 1; i < 50; i++)
+  fe25519_square(&t, &t);
+  for (i = 1; i < 50; i++)
   {
     fe25519_square(&t, &t);
   }
-  /* 2^250 - 2^0 */ fe25519_mul(&t, &t, &z2_50_0);
+  fe25519_mul(&t, &t, &z2_50_0);
 
-  /* 2^251 - 2^1 */ fe25519_square(&t, &t);
-  /* 2^252 - 2^2 */ fe25519_square(&t, &t);
-  /* 2^252 - 3 */ fe25519_mul(r, &t, x);
+  fe25519_square(&t, &t);
+  fe25519_square(&t, &t);
+  fe25519_mul(r, &t, x);
 }
 
 void fe25519_invsqrt(fe25519 *r, const fe25519 *x)
@@ -325,7 +350,7 @@ void fe25519_invsqrt(fe25519 *r, const fe25519 *x)
 
   fe25519_square(&den4, &den2);
   fe25519_mul(&den6, &den2, &den4);
-  fe25519_mul(&t, &den6, x); // r is now x^7
+  fe25519_mul(&t, &den6, x);
 
   fe25519_pow2523(&t, &t);
   fe25519_mul(&t, &t, &den3);
