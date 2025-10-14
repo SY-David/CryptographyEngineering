479d4418b4bb54d66d74675594a4ae81bb111157
Yu <jianao931212@gmail.com>
2025-10-14 23:42:53 +0800

    .

--- ecdh25519/fe25519.s
+++ ecdh25519/fe25519.s
@@ -5,7 +5,7 @@
 .type BIGLIMB, %function
 
 BIGLIMB:
-    push    {r4-r11, lr}
+    push    {r4-r11, lr}@ ...
     sub     sp, sp, #160
 
     mov     r8, sp              @ f limbs base
@@ -25,17 +25,17 @@ BIGLIMB:
     str     r3, [r8, #0]
 
     lsrs    r3, r4, #26
-    orr     r3, r3, r5, lsl #6
+    orr     r3, r3, r5, lsl#6
     bfc     r3, #25, #7
     str     r3, [r8, #4]
 
     lsrs    r3, r5, #19
-    orr     r3, r3, r6, lsl #13
+    orr     r3, r3, r6, lsl#13
     bfc     r3, #26, #6
     str     r3, [r8, #8]
 
     lsrs    r3, r6, #13
-    orr     r3, r3, r7, lsl #19
+    orr     r3, r3, r7, lsl#19
     bfc     r3, #25, #7
     str     r3, [r8, #12]
 
@@ -54,17 +54,17 @@ BIGLIMB:
     str     r3, [r8, #20]
 
     lsrs    r3, r4, #25
-    orr     r3, r3, r5, lsl #7
+    orr     r3, r3, r5, lsl#7
     bfc     r3, #26, #6
     str     r3, [r8, #24]
 
     lsrs    r3, r5, #19
-    orr     r3, r3, r6, lsl #13
+    orr     r3, r3, r6, lsl#13
     bfc     r3, #25, #7
     str     r3, [r8, #28]
 
     lsrs    r3, r6, #12
-    orr     r3, r3, r7, lsl #20
+    orr     r3, r3, r7, lsl#20
     bfc     r3, #26, #6
     str     r3, [r8, #32]
 
@@ -83,17 +83,17 @@ BIGLIMB:
     str     r3, [r9, #0]
 
     lsrs    r3, r4, #26
-    orr     r3, r3, r5, lsl #6
+    orr     r3, r3, r5, lsl#6
     bfc     r3, #25, #7
     str     r3, [r9, #4]
 
     lsrs    r3, r5, #19
-    orr     r3, r3, r6, lsl #13
+    orr     r3, r3, r6, lsl#13
     bfc     r3, #26, #6
     str     r3, [r9, #8]
 
     lsrs    r3, r6, #13
-    orr     r3, r3, r7, lsl #19
+    orr     r3, r3, r7, lsl#19
     bfc     r3, #25, #7
     str     r3, [r9, #12]
 
@@ -112,17 +112,17 @@ BIGLIMB:
     str     r3, [r9, #20]
 
     lsrs    r3, r4, #25
-    orr     r3, r3, r5, lsl #7
+    orr     r3, r3, r5, lsl#7
     bfc     r3, #26, #6
     str     r3, [r9, #24]
 
     lsrs    r3, r5, #19
-    orr     r3, r3, r6, lsl #13
+    orr     r3, r3, r6, lsl#13
     bfc     r3, #25, #7
     str     r3, [r9, #28]
 
     lsrs    r3, r6, #12
-    orr     r3, r3, r7, lsl #20
+    orr     r3, r3, r7, lsl#20
     bfc     r3, #26, #6
     str     r3, [r9, #32]
 
@@ -132,31 +132,35 @@ BIGLIMB:
     @ -------------------------
     @ Compute h = f * g (Comba style via convolution)
     @ -------------------------
-    movs    r4, #0              @ i = 0
+    movs    r4, #0 @ i = 0
 
-1:  movs    r5, #0              @ j = 0
+1:
+    movs    r5, #0              @ j = 0
     movs    r6, #0              @ acc_lo
     movs    r7, #0              @ acc_hi
 
-2:  add     r12, r8, r5, lsl #2
-    ldr     r0, [r12]           @ f_j
+2:
+    add     r12, r8, r5, lsl#2
+    ldr     r0, [r12]@ f_j
     subs    r12, r4, r5
     bge     3f
     add     r12, r12, #10
-    add     r3, r9, r12, lsl #2
+    add     r3, r9, r12, lsl#2
     ldr     r3, [r3]
-    mul     r3, r3, r11         @ 19 * g_k
+    mul     r3, r3, r11 @ 19 * g_k
     b       4f
 
-3:  add     r3, r9, r12, lsl #2
+3:
+    add     r3, r9, r12, lsl#2
     ldr     r3, [r3]
 
-4:  umlal   r6, r7, r0, r3
+4:
+    umlal   r6, r7, r0, r3
     add     r5, r5, #1
     cmp     r5, #10
     blt     2b
 
-    add     r12, r10, r4, lsl #3
+    add     r12, r10, r4, lsl#3
     str     r6, [r12, #0]
     str     r7, [r12, #4]
 
@@ -168,9 +172,9 @@ BIGLIMB:
     @ Carry propagation (same as C implementation)
     @ -------------------------
     movw    r0, #0
-    movt    r0, #0x0200         @ 1 << 25
+    movt    r0, #0x0200 @ 1 << 25
     movw    r1, #0
-    movt    r1, #0x0100         @ 1 << 24
+    movt    r1, #0x0100 @ 1 << 24
 
     @ carry0
     ldr     r4, [r10, #0]
@@ -179,7 +183,7 @@ BIGLIMB:
     adc     r5, r5, #0
 
     lsrs    r6, r4, #26
-    orr     r6, r6, r5, lsl #6
+    orr     r6, r6, r5, lsl#6
 
     ldr     r7, [r10, #8]
     ldr     r8, [r10, #12]
@@ -201,7 +205,7 @@ BIGLIMB:
     adc     r8, r8, #0
 
     lsrs    r6, r7, #25
-    orr     r6, r6, r8, lsl #7
+    orr     r6, r6, r8, lsl#7
 
     ldr     r4, [r10, #16]
     ldr     r5, [r10, #20]
@@ -223,7 +227,7 @@ BIGLIMB:
     adc     r5, r5, #0
 
     lsrs    r6, r4, #26
-    orr     r6, r6, r5, lsl #6
+    orr     r6, r6, r5, lsl#6
 
     ldr     r7, [r10, #24]
     ldr     r8, [r10, #28]
@@ -245,7 +249,7 @@ BIGLIMB:
     adc     r8, r8, #0
 
     lsrs    r6, r7, #25
-    orr     r6, r6, r8, lsl #7
+    orr     r6, r6, r8, lsl#7
 
     ldr     r4, [r10, #32]
     ldr     r5, [r10, #36]
@@ -267,7 +271,7 @@ BIGLIMB:
     adc     r5, r5, #0
 
     lsrs    r6, r4, #26
-    orr     r6, r6, r5, lsl #6
+    orr     r6, r6, r5, lsl#6
 
     ldr     r7, [r10, #40]
     ldr     r8, [r10, #44]
@@ -289,7 +293,7 @@ BIGLIMB:
     adc     r8, r8, #0
 
     lsrs    r6, r7, #25
-    orr     r6, r6, r8, lsl #7
+    orr     r6, r6, r8, lsl#7
 
     ldr     r4, [r10, #48]
     ldr     r5, [r10, #52]
@@ -311,7 +315,7 @@ BIGLIMB:
     adc     r5, r5, #0
 
     lsrs    r6, r4, #26
-    orr     r6, r6, r5, lsl #6
+    orr     r6, r6, r5, lsl#6
 
     ldr     r7, [r10, #56]
     ldr     r8, [r10, #60]
@@ -333,7 +337,7 @@ BIGLIMB:
     adc     r8, r8, #0
 
     lsrs    r6, r7, #25
-    orr     r6, r6, r8, lsl #7
+    orr     r6, r6, r8, lsl#7
 
     ldr     r4, [r10, #64]
     ldr     r5, [r10, #68]
@@ -355,7 +359,7 @@ BIGLIMB:
     adc     r5, r5, #0
 
     lsrs    r6, r4, #26
-    orr     r6, r6, r5, lsl #6
+    orr     r6, r6, r5, lsl#6
 
     ldr     r7, [r10, #72]
     ldr     r8, [r10, #76]
@@ -377,9 +381,9 @@ BIGLIMB:
     adc     r8, r8, #0
 
     lsrs    r6, r7, #25
-    orr     r6, r6, r8, lsl #7
+    orr     r6, r6, r8, lsl#7
 
-    mul     r3, r6, r11        @ carry9 * 19
+    mul     r3, r6, r11 @ carry9 * 19
     ldr     r4, [r10, #0]
     ldr     r5, [r10, #4]
     adds    r4, r4, r3
@@ -402,7 +406,7 @@ BIGLIMB:
     adc     r5, r5, #0
 
     lsrs    r6, r4, #26
-    orr     r6, r6, r5, lsl #6
+    orr     r6, r6, r5, lsl#6
 
     ldr     r7, [r10, #8]
     ldr     r8, [r10, #12]
@@ -424,7 +428,7 @@ BIGLIMB:
     adc     r8, r8, #0
 
     lsrs    r6, r7, #25
-    orr     r6, r6, r8, lsl #7
+    orr     r6, r6, r8, lsl#7
 
     ldr     r4, [r10, #16]
     ldr     r5, [r10, #20]
@@ -446,9 +450,10 @@ BIGLIMB:
     @ -------------------------
     movs    r4, #0
 
-5:  add     r12, r10, r4, lsl #3
+5:
+    add     r12, r10, r4, lsl#3
     ldr     r6, [r12, #0]
-    str     r6, [r2, r4, lsl #2]
+    str     r6, [r2, r4, lsl#2]
     add     r4, r4, #1
     cmp     r4, #10
     blt     5b
