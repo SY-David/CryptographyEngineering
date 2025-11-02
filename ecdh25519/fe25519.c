19efc0aff9709c109b0082ab90b4676af38865dd
Yu <jianao931212@gmail.com>
2025-11-02 22:45:46 +0800

    Try to debug

--- ecdh25519/fe25519.c
+++ ecdh25519/fe25519.c
@@ -531,10 +531,46 @@ static inline void fe25519_square_core(const unsigned char *a,
   int64_t h9 = 2 * (f0 * f9 + f1 * f8 + f2 * f7 + f3 * f6 + f4 * f5);
 
   int64_t carry0 = (h0 + ((int64_t)1 << 25)) >> 26;
-
+  h1 += carry0;
   h0 -= carry0 << 26;
 
-  h[0] = h0;
+  int64_t carry1 = (h1 + ((int64_t)1 << 24)) >> 25;
+  h2 += carry1;
+  h1 -= carry1 << 25;
+
+  int64_t carry2 = (h2 + ((int64_t)1 << 25)) >> 26;
+  h3 += carry2;
+  h2 -= carry2 << 26;
+
+  int64_t carry3 = (h3 + ((int64_t)1 << 24)) >> 25;
+  h4 += carry3;
+  h3 -= carry3 << 25;
+
+  int64_t carry4 = (h4 + ((int64_t)1 << 25)) >> 26;
+  h5 += carry4;
+  h4 -= carry4 << 26;
+
+  int64_t carry5 = (h5 + ((int64_t)1 << 24)) >> 25;
+  h6 += carry5;
+  h5 -= carry5 << 25;
+
+  int64_t carry6 = (h6 + ((int64_t)1 << 25)) >> 26;
+  h7 += carry6;
+  h6 -= carry6 << 26;
+
+  int64_t carry7 = (h7 + ((int64_t)1 << 24)) >> 25;
+  h8 += carry7;
+  h7 -= carry7 << 25;
+
+  int64_t carry8 = (h8 + ((int64_t)1 << 25)) >> 26;
+  h9 += carry8;
+  h8 -= carry8 << 26;
+
+  int64_t carry9 = (h9 + ((int64_t)1 << 24)) >> 25;
+  h0 += carry9 * 19;
+  h9 -= carry9 << 25;
+
+    h[0] = h0;
   h[1] = h1;
   h[2] = h2;
   h[3] = h3;
