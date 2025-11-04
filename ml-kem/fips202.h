ac773219fc6379ce08f35a6d11556d328b9bd0d4
Yu <jianao931212@gmail.com>
2025-11-04 23:18:57 +0800

    Find Good Keccak

--- ml-kem/fips202.h
+++ ml-kem/fips202.h
@@ -11,7 +11,8 @@
 
 #define FIPS202_NAMESPACE(s) pqcrystals_kyber_fips202_ref_##s
 
-typedef struct {
+typedef struct
+{
   uint64_t s[25];
   unsigned int pos;
 } keccak_state;
@@ -40,7 +41,7 @@ void shake256_squeeze(uint8_t *out, size_t outlen, keccak_state *state);
 #define shake256_absorb_once FIPS202_NAMESPACE(shake256_absorb_once)
 void shake256_absorb_once(keccak_state *state, const uint8_t *in, size_t inlen);
 #define shake256_squeezeblocks FIPS202_NAMESPACE(shake256_squeezeblocks)
-void shake256_squeezeblocks(uint8_t *out, size_t nblocks,  keccak_state *state);
+void shake256_squeezeblocks(uint8_t *out, size_t nblocks, keccak_state *state);
 
 #define shake128 FIPS202_NAMESPACE(shake128)
 void shake128(uint8_t *out, size_t outlen, const uint8_t *in, size_t inlen);
