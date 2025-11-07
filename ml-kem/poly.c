c4e9d36812642a68ec860ac880165543afb14560
Yu <jianao931212@gmail.com>
2025-11-07 21:06:49 +0800

    s

--- ml-kem/poly.c
+++ ml-kem/poly.c
@@ -141,20 +141,15 @@ void poly_frommsg(poly *r, const uint8_t msg[KYBER_INDCPA_MSGBYTES])
  **************************************************/
 void poly_tomsg(uint8_t msg[KYBER_INDCPA_MSGBYTES], const poly *a)
 {
-  unsigned int i, j;
-  uint32_t t;
-
-  for (i = 0; i < KYBER_N / 8; i++)
+  for (unsigned i = 0; i < KYBER_N / 8; i++)
   {
     msg[i] = 0;
-    for (j = 0; j < 8; j++)
+    for (unsigned j = 0; j < 8; j++)
     {
-      t = a->coeffs[8 * i + j];
-      // t += ((int16_t)t >> 15) & KYBER_Q;
-      // t  = (((t << 1) + KYBER_Q/2)/KYBER_Q) & 1;
+      uint32_t t = (uint16_t)a->coeffs[8 * i + j]; // 係數輸入需已是 0..q-1
       t <<= 1;
-      t += 1665;
-      t *= 80635;
+      t += 1665;  // 與 4-bit 壓縮相同的中點偏移
+      t *= 80635; // ⌊2^28 / 3329⌋
       t >>= 28;
       t &= 1;
       msg[i] |= t << j;
