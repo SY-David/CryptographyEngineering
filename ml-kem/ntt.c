e5793343cc4a73ae991e62a33cb2798d0abfeeaa
Yu <jianao931212@gmail.com>
2025-11-24 01:07:00 +0800

    d

--- ml-kem/ntt.c
+++ ml-kem/ntt.c
@@ -627,7 +627,7 @@ void invntt_test(int16_t r[256])
   }
   for (int i = 0; i < 256; ++i)
   {
-    r[i] = fqmul(r[i], twist_table[0]);
+    r[i] = fqmul(r[i], twist_table[96]);
   }
 
   for (j = 0; j < 256; j++)
