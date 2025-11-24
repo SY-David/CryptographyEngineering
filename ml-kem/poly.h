a596a45f7c180122b466f99d1a3f8d51a9812487
Yu <jianao931212@gmail.com>
2025-11-24 21:29:21 +0800

    benchmark

--- ml-kem/poly.h
+++ ml-kem/poly.h
@@ -8,7 +8,8 @@
  * Elements of R_q = Z_q[X]/(X^n + 1). Represents polynomial
  * coeffs[0] + X*coeffs[1] + X^2*coeffs[2] + ... + X^{n-1}*coeffs[n-1]
  */
-typedef struct{
+typedef struct __attribute__((aligned(4)))
+{
   int16_t coeffs[KYBER_N];
 } poly;
 
